4966b5d1d4af0fe969f9353a85b7ce3067b95dfb
Mikey Powers <michaelvincentpowers@gmail.com>
2018-05-18 13:38:26 -0700

    init commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const request = require('request');
+
+class App extends Component {
+  componentDidMount() {
+    request(
+      'https://feeds.bbci.co.uk/news/rss.xml',
+      (error, response, body) => {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+      },
+    );
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
